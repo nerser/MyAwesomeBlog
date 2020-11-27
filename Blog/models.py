1b3a1fb70479eebe4e4549b7ac09e49063c14abb
Illia <shkiroillia@gmail.com>
2020-11-27 16:57:48 +0300

    Add a Blog models

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=300)
+    blog_date = models.DateTimeField()
+    blog_text = models.TextField()
+    blog_image = models.ImageField(upload_to='blog_images/')
--- MyAwesomeBlog/settings.py
+++ MyAwesomeBlog/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Events.apps.EventsConfig',
+    'Blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
